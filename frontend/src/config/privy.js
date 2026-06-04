8724626e24369c9397ba66f00c1b56c147702a89
Adebanjo <navigatorabraham17@gmail.com>
2026-06-04 16:52:32 +0100

    update  login/signup method

--- frontend/src/config/privy.js
+++ frontend/src/config/privy.js
@@ -10,7 +10,7 @@ export const privyConfig = {
     landingHeader: 'Welcome to CeloSeer',
     loginMessage: 'Sign in to start predicting World Cup 2026',
   },
-  loginMethods: ['email', 'google', 'twitter', 'apple', 'discord', 'wallet'],
+  loginMethods: ['email', 'wallet'],
   // Privy v3 nests embedded-wallet config under the chain type.
   embeddedWallets: {
     ethereum: {
